1360601071d5699bc22edffab4f0d397c47c280b
Dritriy Golyshev <4dimariK@gmail.com>
2021-11-25 09:01:30 +0300

    Выполнены задания урок 01

---
+++ script.js
@@ -0,0 +1,9 @@
+const title = 'title';
+const screens = 'screens';
+const screenPrice = 'screenPrice';
+const rollback = 'rollback';
+const fullPrice = 'fullPrice';
+const adaptive = 'adaptive';
+
+alert('Hello World!');
+console.log('Hello World!');
