99acc6a0c0a4f5de494c105a451b7e7ed8c2625b
Dritriy Golyshev <4dimariK@gmail.com>
2021-11-26 15:50:38 +0300

    задания 2 урока

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const title = 'title';
-const screens = 'screens';
-const screenPrice = 'screenPrice';
-const rollback = 'rollback';
-const fullPrice = 'fullPrice';
-const adaptive = 'adaptive';
+const title = 'My Project';
+const screens = 'Простые, Сложные, Интерактивные';
+const screenPrice = 500;
+const rollback = 10;
+const fullPrice = 50000;
+const adaptive = true;
 
-alert('Hello World!');
-console.log('Hello World!');
+console.log(`var title: ${typeof title}`);
+console.log(`var fullPrice: ${typeof fullPrice}`);
+console.log(`var adaptive: ${typeof adaptive}`);
+
+console.log(`var screens length: ${screens.length}`);
+
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
+
+console.log(screens.toLowerCase().split(', '));
+
+console.log(`Процент отката - ${fullPrice * (rollback / 100)}`);
