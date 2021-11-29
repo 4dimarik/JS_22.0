5007759882ad05fdb88ac25d6868b520f8706a9e
Dritriy Golyshev <4dimariK@gmail.com>
2021-11-29 09:04:01 +0300

    выполнено задание 3 урока

--- script.js
+++ script.js
@@ -1,19 +1,59 @@
-const title = 'My Project';
-const screens = 'Простые, Сложные, Интерактивные';
-const screenPrice = 500;
+'use strict';
+
+let question = prompt('Как называется ваш проект?');
+let title = !!question ? question : 'Мой проект';
+
+question = prompt('Какие типы экранов нужно разработать?(пример: "Простые, Сложные, Интерактивные")');
+let screens = !!question ? question : 'Не определено';
+
+question = prompt('Сколько будет стоить данная работа?');
+let screenPrice = Number.isInteger(+question) ? +question : 0;
+let adaptive = confirm('Нужен ли адаптив на сайте?');
+
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+question = prompt('Сколько это будет стоить?');
+let servicePrice1 = Number.isInteger(+question) ? +question : 0;
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+question = prompt('Сколько это будет стоить?');
+let servicePrice2 = Number.isInteger(+question) ? +question : 0;
+
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
 const rollback = 10;
-const fullPrice = 50000;
-const adaptive = true;
 
-console.log(`var title: ${typeof title}`);
-console.log(`var fullPrice: ${typeof fullPrice}`);
-console.log(`var adaptive: ${typeof adaptive}`);
+let servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
 
-console.log(`var screens length: ${screens.length}`);
+console.log(`Итоговая стоимость - ${servicePercentPrice}`);
 
-console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
-console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
+switch (true) {
+    case fullPrice >= 30000:
+        console.log('Даем скидку в 10%');
+        break;
+    case fullPrice >= 15000 && fullPrice < 30000:
+        console.log('Даем скидку в 5%');
+        break;
+    case fullPrice >= 0 && fullPrice < 15000:
+        console.log('Скидка не предусмотрена');
+        break;
+    case fullPrice < 0:
+    default:
+        console.log('Что то пошло не так');
+        break;
+}
 
-console.log(screens.toLowerCase().split(', '));
 
-console.log(`Процент отката - ${fullPrice * (rollback / 100)}`);
+
+
+
+// console.log(`var title: ${typeof title}`);
+// console.log(`var fullPrice: ${typeof fullPrice}`);
+// console.log(`var adaptive: ${typeof adaptive}`);
+
+// console.log(`var screens length: ${screens.length}`);
+
+// console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+// console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
+
+// console.log(screens.toLowerCase().split(', '));
+
+// console.log(`Процент отката - ${}`);
