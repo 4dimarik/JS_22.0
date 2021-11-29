b8f52854cfc3c3af26cff1f1d28efa82824117f2
Dritriy Golyshev <4dimariK@gmail.com>
2021-11-29 09:09:23 +0300

    забыл убрать комментарий строк

--- script.js
+++ script.js
@@ -41,19 +41,15 @@ switch (true) {
         break;
 }
 
+console.log(`var title: ${typeof title}`);
+console.log(`var fullPrice: ${typeof fullPrice}`);
+console.log(`var adaptive: ${typeof adaptive}`);
 
+console.log(`var screens length: ${screens.length}`);
 
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
 
+console.log(screens.toLowerCase().split(', '));
 
-// console.log(`var title: ${typeof title}`);
-// console.log(`var fullPrice: ${typeof fullPrice}`);
-// console.log(`var adaptive: ${typeof adaptive}`);
-
-// console.log(`var screens length: ${screens.length}`);
-
-// console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
-// console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
-
-// console.log(screens.toLowerCase().split(', '));
-
-// console.log(`Процент отката - ${}`);
+console.log(`Процент отката - ${fullPrice * (rollback / 100)}`);
